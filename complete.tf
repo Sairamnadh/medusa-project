c3e7b51b986e4bfe4599a3267d33d0b7f2274f22
Sainadh Maripi <132143298+Sairamnadh@users.noreply.github.com>
2024-09-05 11:08:31 +0530

    Update complete.tf

--- complete.tf
+++ complete.tf
@@ -5,14 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
- required_version = ">= 1.0"
+  required_version = ">= 1.0"
 }
 
 provider "aws" {
   region = var.aws_region
-
-
 }
+
 # Data source to get account details
 data "aws_caller_identity" "current" {}
 
@@ -23,6 +22,29 @@ resource "aws_ecr_repository" "medusa_ecr" {
   tags = {
     Name = "medusa-ecr-repository"
   }
+}
+
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # Build and push Docker image using local-exec
@@ -57,7 +79,7 @@ resource "aws_ecs_task_definition" "medusa_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_execution_role.arn  # Add this line
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([{
     name      = "medusa-container"
@@ -76,7 +98,6 @@ resource "aws_ecs_task_definition" "medusa_task" {
     Name = "medusa-task"
   }
 }
-
 
 # VPC
 resource "aws_vpc" "medusa_vpc" {
