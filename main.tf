04c770fd5a5c7b72d3e0aeb8bf7091669f25bbfe
sairamnadh <sairammaripi@gmail.com>
2024-09-05 13:21:43 +0530

    test

--- main.tf
+++ main.tf
@@ -1,41 +1,45 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
-# ECR Repository for Medusa Docker image
-resource "aws_ecr_repository" "medusa_ecr" {
-  name = "medusa-repository"
-}
-
-# IAM Role for ECS Task Execution
 resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecsExecutionRole"
+  name = "ecsExecutionRole1"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Effect = "Allow",
         Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
+          Service = "ecs-tasks.amazonaws.com",
+        },
+        Action = "sts:AssumeRole",
       },
-    ]
+    ],
   })
+
+  tags = {
+    Name = "ecsExecutionRole1"
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role      = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# ECS Cluster
 resource "aws_ecs_cluster" "medusa_cluster" {
   name = "medusa-cluster"
 }
 
-# ECS Task Definition
+resource "aws_ecr_repository" "medusa_ecr" {
+  name = "medusa-repository"
+
+  tags = {
+    Name = "medusa-repository"
+  }
+}
+
 resource "aws_ecs_task_definition" "medusa_task" {
   family                   = "medusa-task"
   network_mode             = "awsvpc"
@@ -62,21 +66,50 @@ resource "aws_ecs_task_definition" "medusa_task" {
   }
 }
 
-# VPC and Networking
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+    security_groups  = [aws_security_group.ecs_security_group.id]
+    assign_public_ip = true
+  }
+
+  tags = {
+    Name = "medusa-service"
+  }
+}
+
 resource "aws_vpc" "medusa_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "medusa-vpc"
+  }
 }
 
 resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.medusa_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "medusa-subnet-1"
+  }
 }
 
 resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.medusa_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "medusa-subnet-2"
+  }
 }
 
 resource "aws_security_group" "ecs_security_group" {
@@ -98,24 +131,5 @@ resource "aws_security_group" "ecs_security_group" {
 
   tags = {
     Name = "medusa-security-group"
-  }
-}
-
-# ECS Service
-resource "aws_ecs_service" "medusa_service" {
-  name            = "medusa-service"
-  cluster         = aws_ecs_cluster.medusa_cluster.id
-  task_definition = aws_ecs_task_definition.medusa_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-    security_groups  = [aws_security_group.ecs_security_group.id]
-    assign_public_ip = true
-  }
-
-  tags = {
-    Name = "medusa-service"
   }
 }
