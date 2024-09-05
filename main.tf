f43019577109ded4a146d99e22dc647d2b3d9c4f
sairamnadh <sairammaripi@gmail.com>
2024-09-05 13:14:32 +0530

    4

--- main.tf
+++ main.tf
@@ -1,14 +1,33 @@
-# Terraform Configuration
-
 provider "aws" {
   region = var.aws_region
 }
 
-data "aws_caller_identity" "current" {}
-
-# ECR Repository
+# ECR Repository for Medusa Docker image
 resource "aws_ecr_repository" "medusa_ecr" {
   name = "medusa-repository"
+}
+
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # ECS Cluster
@@ -27,7 +46,7 @@ resource "aws_ecs_task_definition" "medusa_task" {
 
   container_definitions = jsonencode([{
     name      = "medusa-container"
-    image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${aws_ecr_repository.medusa_ecr.name}:latest"
+    image     = "${aws_ecr_repository.medusa_ecr.repository_url}:latest"
     cpu       = 0
     memory    = 512
     essential = true
@@ -43,28 +62,43 @@ resource "aws_ecs_task_definition" "medusa_task" {
   }
 }
 
-# IAM Role
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecsExecutionRole"
-  
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
+# VPC and Networking
+resource "aws_vpc" "medusa_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
-# IAM Role Policy Attachment
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.medusa_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.medusa_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_security_group" "ecs_security_group" {
+  vpc_id = aws_vpc.medusa_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "medusa-security-group"
+  }
 }
 
 # ECS Service
