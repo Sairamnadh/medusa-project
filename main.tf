29b6fb4b758c0893f19475bbd65dbb23ac819bff
sairamnadh <sairammaripi@gmail.com>
2024-09-05 13:07:50 +0530

    3

--- main.tf
+++ main.tf
@@ -1,87 +1,22 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.0"
-}
+# Terraform Configuration
 
 provider "aws" {
   region = var.aws_region
 }
 
-# Data source to get account details
 data "aws_caller_identity" "current" {}
 
-# ECR Repository for Medusa Docker image
+# ECR Repository
 resource "aws_ecr_repository" "medusa_ecr" {
   name = "medusa-repository"
-
-  tags = {
-    Name = "medusa-ecr-repository"
-  }
-
-  lifecycle {
-    ignore_changes = [name]
-  }
 }
 
-# Build and push Docker image using local-exec
-resource "null_resource" "docker_build_and_push" {
-  provisioner "local-exec" {
-    command = <<EOT
-      # Log in to AWS ECR
-      aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
-
-      # Build Docker image
-      docker build -t medusa-image .
-
-      # Tag Docker image
-      docker tag medusa-image:latest ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${aws_ecr_repository.medusa_ecr.name}:latest
-
-      # Push Docker image to ECR
-      docker push ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${aws_ecr_repository.medusa_ecr.name}:latest
-    EOT
-  }
-
-  depends_on = [aws_ecr_repository.medusa_ecr]
-}
-
-# ECS Cluster for Medusa
+# ECS Cluster
 resource "aws_ecs_cluster" "medusa_cluster" {
   name = "medusa-cluster"
 }
 
-# IAM Role for ECS Task Execution
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecsExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action    = "sts:AssumeRole",
-        Effect    = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com",
-        },
-      },
-    ],
-  })
-
-  tags = {
-    Name = "ecsExecutionRole"
-  }
-}
-
-# IAM Role Policy Attachment
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
+# ECS Task Definition
 resource "aws_ecs_task_definition" "medusa_task" {
   family                   = "medusa-task"
   network_mode             = "awsvpc"
@@ -108,60 +43,31 @@ resource "aws_ecs_task_definition" "medusa_task" {
   }
 }
 
-# VPC
-resource "aws_vpc" "medusa_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "medusa-vpc"
-  }
+# IAM Role
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsExecutionRole"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-# Subnets
-resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.medusa_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "medusa-subnet-1"
-  }
+# IAM Role Policy Attachment
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.medusa_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "medusa-subnet-2"
-  }
-}
-
-# Security Group for ECS tasks
-resource "aws_security_group" "ecs_security_group" {
-  vpc_id = aws_vpc.medusa_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "medusa-security-group"
-  }
-}
-
-# ECS Service for Medusa
+# ECS Service
 resource "aws_ecs_service" "medusa_service" {
   name            = "medusa-service"
   cluster         = aws_ecs_cluster.medusa_cluster.id
